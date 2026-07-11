c0909f6240de722bacd4643bddedbcc22831467a
0348861186 <tanlhu.lkt@gmail.com>
2026-07-11 10:36:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib
-matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 from datetime import datetime, timedelta
